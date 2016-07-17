1e79cb175296e94da3b2dcf6245d1b2c630e5b25
Tiago Oliveira <tiago.co@gmail.com>
2016-07-17 01:36:34 +0100

    Setup gulp build

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+/*eslint-env node*/
+const fs = require('fs');
+const path = require('path');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const source = require('vinyl-source-stream');
+const rollup = require('rollup-stream');
+const buble = require('rollup-plugin-buble');
+const del = require('del');
+const sequence = require('run-sequence');
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
+const dependencies = Object.keys(pkg.dependencies);
+const banner = `/*! ${pkg.name} v${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n`;
+const moduleName = 'ngXhrPromisify';
+
+function bundle(format) {
+  return rollup({
+    entry: 'src/index.js',
+    plugins: [ buble() ],
+    external: dependencies,
+    moduleName: moduleName,
+    banner: banner,
+    sourceMap: false,
+    format: format || 'umd'
+  });
+}
+
+gulp.task('bundle:es', () => {
+  const target = pkg['jsnext:main'];
+  const file = path.basename(target);
+  const dir = path.dirname(target);
+  return bundle('es')
+    .pipe(source(file))
+    .pipe(gulp.dest(dir));
+});
+
+gulp.task('bundle:umd', () => {
+  const target = pkg['main'];
+  const file = path.basename(target);
+  const dir = path.dirname(target);
+  return bundle('umd')
+    .pipe(source(file))
+    .pipe(gulp.dest(dir));
+});
+
+gulp.task('lint', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('clean', () => {
+  return del('dist');
+});
+
+gulp.task('bundle', ['bundle:es', 'bundle:umd']);
+
+gulp.task('build', (callback) => {
+  sequence('lint', 'clean', 'bundle', callback);
+});
+
+gulp.task('default', ['build']);
