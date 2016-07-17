8c7a929312e685d4643d3cd452b78dabfcc3804b
Tiago Oliveira <tiago.co@gmail.com>
2016-07-17 01:45:36 +0100

    Minify UMD bundle
    Closes #3

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const buffer = require('vinyl-buffer');
 const source = require('vinyl-source-stream');
 const rollup = require('rollup-stream');
 const buble = require('rollup-plugin-buble');
@@ -41,6 +45,15 @@ gulp.task('bundle:umd', () => {
   const dir = path.dirname(target);
   return bundle('umd')
     .pipe(source(file))
+    .pipe(gulp.dest(dir))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(uglify({ preserveComments: 'license' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('.', {
+      includeContent: false,
+      mapFile: name => name.replace(/\.js\.map$/, '.map')
+    }))
     .pipe(gulp.dest(dir));
 });
 
